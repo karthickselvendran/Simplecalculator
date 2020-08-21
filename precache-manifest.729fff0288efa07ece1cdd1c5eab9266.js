d1cd06afb22a983c70354d2f7eca9a05649d6f10
karthickselvendran <selvendrankarthick@gmail.com>
2020-08-21 19:49:38 +0530

    Updates

---
+++ precache-manifest.729fff0288efa07ece1cdd1c5eab9266.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d701622366708518e670c0da598a9d63",
+    "url": "/simplecalculator/index.html"
+  },
+  {
+    "revision": "5ca69de988fff97a3093",
+    "url": "/simplecalculator/static/css/main.1fc973a9.chunk.css"
+  },
+  {
+    "revision": "83a903924baa4caaccaa",
+    "url": "/simplecalculator/static/js/2.a6f7bbcd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/simplecalculator/static/js/2.a6f7bbcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ca69de988fff97a3093",
+    "url": "/simplecalculator/static/js/main.b396f683.chunk.js"
+  },
+  {
+    "revision": "6b342897a007a42c6017",
+    "url": "/simplecalculator/static/js/runtime-main.68c2ebd5.js"
+  }
+]);
